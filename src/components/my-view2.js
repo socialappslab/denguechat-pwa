84b0106bda0630f4367f5eb4bec67b608976877e
Carlos <aquinoqcd29@gmail.com>
2018-07-23 10:55:16 -0400

    Add Cards to Participantes section.

--- src/components/my-view2.js
+++ src/components/my-view2.js
@@ -8,6 +8,8 @@ Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
 
+import '@polymer/paper-card/paper-card.js';
+import '@polymer/paper-button/paper-button.js';
 import { html } from '@polymer/lit-element';
 import { PageViewElement } from './page-view-element.js';
 import { connect } from 'pwa-helpers/connect-mixin.js';
@@ -31,6 +33,11 @@ import './counter-element.js';
 import { SharedStyles } from './shared-styles.js';
 
 class MyView2 extends connect(store)(PageViewElement) {
+
+  _click(url) {
+      console.log(url)
+  }
+
   _render(props) {
     // return html`
     //   ${SharedStyles}
@@ -56,8 +63,26 @@ class MyView2 extends connect(store)(PageViewElement) {
     // `;
     return html`
       ${SharedStyles}
-      <section>
+      <section style="text-align: center;">
         <h3>Lista de Coordinadores</h3>
+        <paper-card image="https://scontent.fasu5-1.fna.fbcdn.net/v/t1.0-1/p320x320/33530514_10215077595388586_8757841944685576192_n.jpg?_nc_cat=0&oh=d01c6f64819a2149e2373b9b6c316317&oe=5BD4B73D" heading="Cristhian Parra" alt="Emmental" style="text-align: center;  width: 300px;  margin: 8px;">
+          <div class="card-content" style="text-align: left;">
+          Cristhian Parra, es colaborador en AppCivist.
+          </div>
+          <div class="card-actions" style="text-align: right;">
+            <paper-button><a href="https://www.facebook.com/cristhian.parra" target="_blank" style="color: #000000; font-size: 17px; text-decoration: none; font-family: Roboto, Arial, sans-serif;
+            font-weight: 500;">Ver</a></paper-button>
+          </div>
+        </paper-card>
+        <paper-card image="https://scontent.fasu5-1.fna.fbcdn.net/v/t1.0-1/c73.0.320.320/p320x320/35198996_237698383482015_6506930085424005120_n.jpg?_nc_cat=0&oh=af1ea7fa4611444f14034cafbcba65dd&oe=5BD57AA0" heading="Laura González" alt="Emmental" style="text-align: center; width: 300px;  margin: 8px;">
+          <div class="card-content" style="text-align: left;">
+            Laura González, es coordinadora en Mil Solidarios.
+          </div>
+          <div class="card-actions" style="text-align: right;">
+          <paper-button><a href="https://www.facebook.com/profile.php?id=100017255658944" target="_blank" style="color: #000000; font-size: 17px; text-decoration: none; font-family: Roboto, Arial, sans-serif;
+          font-weight: 500;">Ver</a></paper-button>
+          </div>
+        </paper-card>
       </section>
       <section>
         <h3>Lista de Voluntarios</h3>
